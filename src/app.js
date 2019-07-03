a6b6db3f94d68fb38cf4fb878c5c058724834133
Przemek <przemek.tropicalsun@gmail.com>
2019-07-03 13:00:53 +0200

    fixed profile data loading

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
-import './assets/scss/app.scss';
-import $ from 'cash-dom';
+import "./assets/scss/app.scss";
+import $ from "cash-dom";
+import "isomorphic-fetch";
+import { polyfill } from "es6-promise";
 
+polyfill();
 
 export class App {
   initializeApp() {
-    let self = this;
+    $(".load-username").on("click", e => {
+      const userName = $(".username.input").val();
 
-    $('.load-username').on('click', function (e) {
-      let userName = $('.username.input').val();
-
-      fetch('https://api.github.com/users/' + userName)
-        .then((response)=> {response.json})
-        .then(function (body) {
-          self.profile = body;
-          self.update_profile();
-        })
-
-    })
-
+      fetch(`https://api.github.com/users/${userName}`)
+        .then(response => response.json())
+        .then(body => {
+          this.profile = body;
+          this.updateProfile();
+        });
+    });
   }
 
-  update_profile() {
-    $('#profile-name').text($('.username.input').val())
-    $('#profile-image').attr('src', this.profile.avatar_url)
-    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
-    $('#profile-bio').text(this.profile.bio || '(no information)')
+  updateProfile() {
+    $("#profile-name").text($(".username.input").val());
+    $("#profile-image").attr("src", this.profile.avatar_url);
+    $("#profile-url")
+      .attr("href", this.profile.html_url)
+      .text(this.profile.login);
+    $("#profile-bio").text(this.profile.bio || "(no information)");
   }
 }
